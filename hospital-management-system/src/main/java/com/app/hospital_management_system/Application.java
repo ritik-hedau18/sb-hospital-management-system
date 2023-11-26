df08ce6971e74b333655b6a087f553f35f9045d3
ritik-hedau18 <ritikhedau253@gmail.com>
2023-11-26 12:07:00 +0530

    feat: integrate mail service and alert notification templates

---
+++ hospital-management-system/src/main/java/com/app/hospital_management_system/Application.java
@@ -0,0 +1,14 @@
+package com.app.hospital_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+// minor tweak: performance check updates
+}
